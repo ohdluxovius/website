70129d436bcfcf18eb0f410ddb561b244671121a
Jonathan Matarazzi <git@jonathanm.nl>
2025-05-07 17:02:49 +0200

    Fix layout issues

--- app/components/IntroSection.vue
+++ app/components/IntroSection.vue
@@ -8,7 +8,7 @@ const {data: page} = useAsyncData('intro', () => {
 
 <template>
   <section id="about" class="py-40 text-center">
-    <div class="container">
+    <div class="container m-auto">
       <div class="row justify-content-center">
         <div class="lg:w-8/12 lg:m-auto">
           <h2 class="text-4xl font-normal">Onafhankelijk Heeren Dispuut Luxovius</h2>
