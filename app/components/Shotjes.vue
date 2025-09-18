f091e797a4ea2d045353a10d8f0133ccf40c1776
Jonathan Matarazzi <git@jonathanm.nl>
2025-09-18 10:28:36 +0200

    Change shotjes page height from lvh to svh for better mobile compatibility

--- app/components/Shotjes.vue
+++ app/components/Shotjes.vue
@@ -7,7 +7,7 @@ const {data: page} = useAsyncData('shotjes', () => {
 </script>
 
 <template>
-  <div class="absolute w-full h-lvh bg-black text-white text-center">
+  <div class="absolute w-full h-svh bg-black text-white text-center">
     <div class="absolute top-1/2 left-1/2 -translate-1/2 scale-150">
       <div>
         <h3 class="text-xl font-light">Kunnen we shotjes doen?</h3>
