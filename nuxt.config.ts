a4f88d1c2d9a12f62fc4709eac5889971f97a3f5
Jonathan Matarazzi <git@jonathanm.nl>
2026-02-14 12:24:56 +0100

    Fix config syntax and lichting prop typing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
         { rel: 'shortcut icon', href: '/favicon.ico' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'manifest', href: '/site.webmanifest' }.
+        { rel: 'manifest', href: '/site.webmanifest' }
       ],
       meta: [
         { name: 'apple-mobile-web-app-title', content: 'Luxovius' },
